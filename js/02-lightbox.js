1251efd7f230cf6867e598e1b0014d3109a7ba3a
natalikoyani <natalikagami@gmail.com>
2023-07-14 23:16:19 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+    .map(galleryItem => `<li class="gallery__item">
+   <a class="gallery__link" href=${galleryItem.original}>
+      <img class="gallery__image" src=${galleryItem.preview} alt=${galleryItem.description} />
+   </a>
+</li>`)
+    .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay: 250, });
